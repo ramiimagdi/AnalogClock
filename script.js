5fb1e9419cd03062dd148356bb19541cbe6c3dd8
Rami Magdi <rami-magdi@hotmail.com>
2020-10-30 19:09:10 +0200

    Fixed a typo

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-let hrPostition = 20;
-let minPositiion = 130;
+let hrPosition = 20;
+let minPosition = 130;
 let secPosition = 267;
 
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-MINUTEHAND.style.transform = "rotate(" + minPositiion + "deg)";
+MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
 SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
