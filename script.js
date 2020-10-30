5bcd07658a1680704bd7a3137f3587315868d237
Rami Magdi <rami-magdi@hotmail.com>
2020-10-30 19:05:07 +0200

    Changed the arms of the clock on JS file

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+let hrPostition = 20;
+let minPositiion = 130;
+let secPosition = 267;
+
+HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+MINUTEHAND.style.transform = "rotate(" + minPositiion + "deg)";
+SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
