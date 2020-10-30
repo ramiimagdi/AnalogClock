e05b06333943423888aa3e793e7328d3aba51bdb
Rami Magdi <rami-magdi@hotmail.com>
2020-10-30 20:19:42 +0200

    Added the final touches to the project to make sure everything is running as expected. Nothing further should be added here.

--- script.js
+++ script.js
@@ -2,18 +2,23 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
+var date = new Date();
+console.log(date);
+
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
+
+let hrPosition = (hr * 360 / 12) + (min * (360 * 60) / 12);
+let minPosition = (min * 360 / 60) + (sec * (360 / 60) / 60) ;
+let secPosition = sec * 360 / 60 ;
+
 function runTheClock() {
-    var date = new Date();
-    console.log(date);
 
-    let hr = date.getHours();
-    let min = date.getMinutes();
-    let sec = date.getSeconds();
-    console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
-
-    let hrPosition = (hr * 360 / 12) + (min * (360 * 60) / 12);
-    let minPosition = (min * 360 / 60) + (sec * (360 / 60) / 60) ;
-    let secPosition = sec * 360 / 60 ;
+    secPosition = secPosition + 6;
+    minPosition = minPosition + 0.1;
+    hrPosition = hrPosition + (1 / 120);
 
     HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
     MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
